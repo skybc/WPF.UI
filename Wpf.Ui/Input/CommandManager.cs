0ac6d69b306a16161d4acb7a75b20f7b082a39c1
dream <dreammisc@outlook.com>
2025-09-25 22:50:12 +0800

    撤销，重做功能。

--- Wpf.Ui/Input/CommandManager.cs
+++ Wpf.Ui/Input/CommandManager.cs
@@ -1,13 +1,34 @@
-﻿namespace Wpf.Ui.Input
+﻿// <copyright>
+//   Copyright (c) 2025 skybc.
+//   All rights reserved.
+// </copyright>
+// <license>
+//   Licensed under the project license.
+// </license>
+namespace Wpf.Ui.Input
 {
     /// <summary>
-    ///  命令管理器，支持命令的执行、撤销和重做
+    /// Command manager supporting execute, undo and redo.
     /// </summary>
     public class CommandManager
     {
-
         private readonly Stack<IUndoableCommand> undoStack = new Stack<IUndoableCommand>();
         private readonly Stack<IUndoableCommand> redoStack = new Stack<IUndoableCommand>();
+
+        /// <summary>
+        /// Occurs when undo/redo stacks change to refresh UI state.
+        /// </summary>
+        public event EventHandler Changed;
+
+        /// <summary>
+        /// Gets a value indicating whether an undo operation is available.
+        /// </summary>
+        public bool CanUndo => this.undoStack.Any();
+
+        /// <summary>
+        /// Gets a value indicating whether a redo operation is available.
+        /// </summary>
+        public bool CanRedo => this.redoStack.Any();
 
         /// <summary>
         /// zh: 执行一个可撤销的命令，并将其添加到撤销栈中.
@@ -18,6 +39,7 @@
             cmd.Execute();
             this.undoStack.Push(cmd);
             this.redoStack.Clear();
+            this.Changed?.Invoke(this, EventArgs.Empty);
         }
 
         /// <summary>
@@ -30,6 +52,7 @@
                 var cmd = this.undoStack.Pop();
                 cmd.Undo();
                 this.redoStack.Push(cmd);
+                this.Changed?.Invoke(this, EventArgs.Empty);
             }
         }
 
@@ -43,10 +66,22 @@
                 var cmd = this.redoStack.Pop();
                 cmd.Execute();
                 this.undoStack.Push(cmd);
+                this.Changed?.Invoke(this, EventArgs.Empty);
             }
         }
-    }
 
+        /// <summary>
+        /// Clears the undo/redo history.
+        /// </summary>
+        public void Clear()
+        {
+            this.undoStack.Clear();
+            this.redoStack.Clear();
+            this.Changed?.Invoke(this, EventArgs.Empty);
+        }
+    }
 }
+
+
 
 
